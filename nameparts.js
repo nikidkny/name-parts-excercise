c46c69a8c246cef872d34f8d725b6c48485a1400
Nikolett Dékány <niko703j@stud.kea.dk>
2022-08-30 13:03:38 +0200

    finsihed

---
+++ nameparts.js
@@ -0,0 +1,18 @@
+const name = "Peter Heronimous Lind";
+// const firstNameEnd = name.indexOf(" ");
+const firstName = name.substring(0, name.indexOf(" "));
+const middleName = name.substring(name.indexOf(" ") + 1, name.lastIndexOf(" "));
+const lastName = name.substring(name.lastIndexOf(" ") + 1);
+// const lastNameStart = name.indexOf("");
+// const lastName = name.substring();
+// const middleName = name.substring(firstNameEnd, middleNameEnd);
+// const lastName = name.;
+// console.log(`First name is: ${name.}`)
+console.log(firstName);
+// console.log(middleNameEnd);
+console.log(middleName);
+console.log(lastName);
+// where the middleName would end up being multimple names like here "Percival Wulfric Brian"
+const name1 = "Albus Percival Wulfric Brian Dumbledore";
+const middleNames = name1.substring(name1.indexOf(" ") + 1, name1.lastIndexOf(" "));
+console.log(middleNames);
