d6dc9d0999b99f6e6f6ed9173b19e03b59da2ed4
Nikolett Dékány <niko703j@stud.kea.dk>
2022-09-05 13:43:55 +0200

    added inside a function

--- capitalization.js
+++ capitalization.js
@@ -1,3 +1,7 @@
-const name1 = "NIKI";
+const name1 = "MaDEliNE";
 const firstLetter = name1.charAt(0).toUpperCase() + name1.substring(1).toLowerCase();
 console.log(firstLetter);
+capitalise(name1);
+function capitalise(name1) {
+  console.log(`My name is ${name1.charAt(0).toUpperCase() + name1.substring(1).toLowerCase()}`);
+}
